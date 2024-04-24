cd0818459fabd861d5a8638c65a8363de79070fd
OleksandrBalashov <aleksandr.balashov.sax@gmai.com>
2024-04-24 07:55:39 +0200

    fix index

--- src/index.jsx
+++ src/index.jsx
@@ -1,25 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import CatalogPage from "./pages/CatalogPage";
-import { Provider } from "react-redux";
-import store from "./redux/store";
-import "modern-normalize/modern-normalize.css";
-import "./index.scss";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import CatalogPage from './pages/CatalogPage';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import 'modern-normalize/modern-normalize.css';
+import './index.scss';
 
-import FavoritesPage from "./pages/FavoritesPage";
-import Header from "./components/Header";
+import FavoritesPage from './pages/FavoritesPage';
+import Header from './components/Header';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/catalog' element={<CatalogPage />} />
-        <Route path='/favorites' element={<FavoritesPage />} />
+        <Route path="/" index element={<HomePage />} />
+        <Route path="/catalog" element={<CatalogPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
       </Routes>
     </BrowserRouter>
   </Provider>
